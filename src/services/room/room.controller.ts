e03da5dcad257848295adbcf35b9fb70f29858cd
Fi44er <vaceslavmalugin81@gmail.com>
2024-03-07 02:15:06 +0500

    add

--- src/services/room/room.controller.ts
+++ src/services/room/room.controller.ts
@@ -4,21 +4,22 @@ import { FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { editFileName, imageFileFilter } from 'lib/utils/image-upload';
 import { Public } from 'lib/decorators/public.decorator';
+import { RoomService } from './room.service';
 
 @Public()
 @Controller('room')
 export class RoomController {
-    @Post('add-room')
-        @UseInterceptors(
-            FilesInterceptor('image', 10, {
-                storage: diskStorage({
-                destination: './uploads',
-                filename: editFileName,
-                }),
-                fileFilter: imageFileFilter,
-            }),
-        )
-    async AddRoom(files: Express.Multer.File) {
-        return {files}
-    }     
+    constructor(private readonly photoService: RoomService) {}
+    @Post('upload')
+  @UseInterceptors(FilesInterceptor('image', 10, {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: editFileName,
+    }),
+    fileFilter: imageFileFilter,
+  }))
+  async uploadPhotos(@UploadedFiles() files) {
+    await this.photoService.savePhotosToServer(files);
+    return 'Photos uploaded successfully';
+  }
 }
--- src/services/room/room.service.ts
+++ src/services/room/room.service.ts
@@ -1,13 +1,17 @@
+// photo.service.ts
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { AddRoomDto } from './DTO/AddRoom.dto';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class RoomService {
-    constructor(
-        private readonly prismaService: PrismaService
-    ) {}
-
-    async AddRoom(dto: AddRoomDto, files: Express.Multer.File) {
+  async savePhotosToServer(files) {
+    // Обработка и сохранение загруженных файлов
+    for (const file of files) {
+        console.log(typeof file.filename);
+        
+      const filePath = path.join('./uploads', String());
+      fs.writeFileSync(filePath, file.buffer);
     }
+  }
 }
