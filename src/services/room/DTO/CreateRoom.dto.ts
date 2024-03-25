fa9db4a3b9f1ec1ee1f47087ee6f289fba839cfa
Fi44er <vaceslavmalugin81@gmail.com>
2024-03-25 13:57:12 +0500

    fixed createRoomDto

--- src/services/room/DTO/CreateRoom.dto.ts
+++ src/services/room/DTO/CreateRoom.dto.ts
@@ -1,24 +1,29 @@
 import { ApiProperty } from "@nestjs/swagger"
-import { IsNumber, IsString } from "class-validator"
+import { IsNotEmpty, IsNumber, IsString } from "class-validator"
 
 export class CreateRoomDto {
     @ApiProperty()
+    @IsNotEmpty()
     @IsString()
     address: string
 
     @ApiProperty()
+    @IsNotEmpty()
     @IsString()
     name: string
 
     @ApiProperty()
+    @IsNotEmpty()
     @IsString()
     description: string
 
     @ApiProperty()
+    @IsNotEmpty()
     @IsNumber()
     price: number
 
     @ApiProperty()
+    @IsNotEmpty()
     @IsNumber()
     places: number
 }
